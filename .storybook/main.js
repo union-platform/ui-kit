a33818b8ffa5c6f19ae2e6f8c0bf6d7e10f76aa8
Artyom Alekseev <contact@artyomalekseev.com>
2022-02-18 19:48:16 +0300

    Prepare repo for development (#2)
    * Prepare repo for development
    
    Problem: to start development we need to prepare repository
    
    Solution: We added licenses, templates and required dependencies to
    start developing the UI-kit
    
    * fixup! Prepare repo for development
    
    * fixup! Prepare repo for development

---
+++ .storybook/main.js
@@ -0,0 +1,17 @@
+// SPDX-FileCopyrightText: 2022 Union
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
+module.exports = {
+  "stories": [
+    "../src/**/*.stories.mdx",
+    "../src/**/*.stories.@(js|jsx|ts|tsx)"
+  ],
+  "addons": [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions",
+    "@storybook/preset-create-react-app"
+  ],
+  "framework": "@storybook/react"
+}
---
+++ .storybook/preview.js
@@ -0,0 +1,13 @@
+// SPDX-FileCopyrightText: 2022 Union
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
+export const parameters = {
+  actions: { argTypesRegex: "^on[A-Z].*" },
+  controls: {
+    matchers: {
+      color: /(background|color)$/i,
+      date: /Date$/,
+    },
+  },
+}
