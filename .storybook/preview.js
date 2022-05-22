56c61c53c4d17d7fcb129772d3fde1b9c424562a
rxln <contact@artyomalekseev.com>
2022-05-23 00:06:27 +0300

    [#60] Show Library Version in Storybook
    Problem:
    It's convinient to see library version in storybook
    
    Solution:
    Version now shows in title and side-panel

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,10 +1,10 @@
 // SPDX-FileCopyrightText: 2022 Union
 //
 // SPDX-License-Identifier: AGPL-3.0-or-later
+import { DocsPage, DocsContainer } from '@storybook/addon-docs';
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
-  viewMode: 'docs',
   controls: {
     matchers: {
       color: /(background|color)$/i,
